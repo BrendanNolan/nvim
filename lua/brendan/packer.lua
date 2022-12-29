8208fc611c687d05ee026fddb7ad7f6d4294a3a6
Brendan Nolan <bnolan23@gmail.com>
2022-12-29 18:53:56 +0000

    Add Harpoon

--- lua/brendan/packer.lua
+++ lua/brendan/packer.lua
@@ -18,6 +18,7 @@ return require('packer').startup(function(use)
 
   use('nvim-treesitter/nvim-treesitter', {run = 'TSUpdate'})
   use('nvim-treesitter/playground')
+  use('theprimeagen/harpoon')
 
 end)
 
--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -74,6 +74,11 @@ end
 time([[try_loadstring definition]], false)
 time([[Defining packer_plugins]], true)
 _G.packer_plugins = {
+  harpoon = {
+    loaded = true,
+    path = "/home/brendan/.local/share/nvim/site/pack/packer/start/harpoon",
+    url = "https://github.com/theprimeagen/harpoon"
+  },
   ["nvim-treesitter"] = {
     loaded = true,
     path = "/home/brendan/.local/share/nvim/site/pack/packer/start/nvim-treesitter",
@@ -83,6 +88,11 @@ _G.packer_plugins = {
     loaded = true,
     path = "/home/brendan/.local/share/nvim/site/pack/packer/start/packer.nvim",
     url = "https://github.com/wbthomason/packer.nvim"
+  },
+  playground = {
+    loaded = true,
+    path = "/home/brendan/.local/share/nvim/site/pack/packer/start/playground",
+    url = "https://github.com/nvim-treesitter/playground"
   },
   ["plenary.nvim"] = {
     loaded = true,
