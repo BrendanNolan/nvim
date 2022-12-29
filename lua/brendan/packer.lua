0e76ff45c9b35fa8dfecd15d63dbf109184cb5c8
Brendan Nolan <bnolan23@gmail.com>
2022-12-29 18:23:54 +0000

    Added Colours

--- lua/brendan/packer.lua
+++ lua/brendan/packer.lua
@@ -8,5 +8,13 @@ return require('packer').startup(function(use)
 	 requires = { {'nvim-lua/plenary.nvim'} }
   }
 
+  use({
+      'rose-pine/neovim',
+      as = 'rose-pine',
+      config = function()
+          vim.cmd('colorscheme rose-pine')
+      end
+  })
+
 end)
 
