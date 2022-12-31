39474384059070c478dd2e60251694957cff543a
Brendan Nolan <bnolan23@gmail.com>
2022-12-31 01:50:19 +0000

    Add Undotree

--- lua/brendan/packer.lua
+++ lua/brendan/packer.lua
@@ -19,6 +19,7 @@ return require('packer').startup(function(use)
   use('nvim-treesitter/nvim-treesitter', {run = 'TSUpdate'})
   use('nvim-treesitter/playground')
   use('theprimeagen/harpoon')
+  use('mbbill/undotree')
 
 end)
 
