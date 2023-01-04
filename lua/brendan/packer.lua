dc4d7784be51eee0c4c376aa38ca3be1594120b2
Brendan Nolan <bnolan23@gmail.com>
2023-01-04 16:20:11 +0000

    Add vscode colours

--- lua/brendan/packer.lua
+++ lua/brendan/packer.lua
@@ -17,6 +17,7 @@ return require('packer').startup(function(use)
   })
 
   use { "ellisonleao/gruvbox.nvim" }
+  use { "Mofiqul/vscode.nvim" }
 
   use('nvim-treesitter/nvim-treesitter', {run = 'TSUpdate'})
   use('nvim-treesitter/playground')
--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -195,6 +195,11 @@ _G.packer_plugins = {
     loaded = true,
     path = "/home/brendan/.local/share/nvim/site/pack/packer/start/vim-fugitive",
     url = "https://github.com/tpope/vim-fugitive"
+  },
+  ["vscode.nvim"] = {
+    loaded = true,
+    path = "/home/brendan/.local/share/nvim/site/pack/packer/start/vscode.nvim",
+    url = "https://github.com/Mofiqul/vscode.nvim"
   }
 }
 
