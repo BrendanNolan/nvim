52a1a9431e266ed997007d450dba4f0c36206910
Brendan Nolan <bnolan23@gmail.com>
2023-01-09 21:49:58 +0000

    Set Up Debugger For Rust

--- after/plugin/rusttools.lua
+++ after/plugin/rusttools.lua
@@ -1,5 +1,9 @@
 local rt = require("rust-tools")
 
+local extension_path = vim.env.HOME .. '/.vscode/extensions/vadimcn.vscode-lldb-1.8.1/'
+local codelldb_path = extension_path .. 'adapter/codelldb'
+local liblldb_path = extension_path .. 'lldb/lib/liblldb.so'
+
 rt.setup({
   tools = { autoSetHints = false, inlay_hints = { auto = false },},
   server = {
@@ -8,6 +12,10 @@ rt.setup({
       Setup_LSP_Keymaps(opts)
     end,
     cmd = { "rustup", "run", "stable", "rust-analyzer" },
+  },
+  dap = {
+    adapter = require('rust-tools.dap').get_codelldb_adapter(
+      codelldb_path, liblldb_path)
   },
 })
 
