c8ccd835dcfc7e19ad10dfdd09ae2f08db6d82a0
Brendan Nolan <bnolan23@gmail.com>
2023-01-12 18:26:29 +0000

    Let User Manually Close dapui Session

--- after/plugin/dapui.lua
+++ after/plugin/dapui.lua
@@ -4,10 +4,6 @@ local dap, dapui = require("dap"), require("dapui")
 dap.listeners.after.event_initialized["dapui_config"] = function()
   dapui.open()
 end
-dap.listeners.before.event_terminated["dapui_config"] = function()
-  dapui.close()
-end
-dap.listeners.before.event_exited["dapui_config"] = function()
-  dapui.close()
-end
+
+vim.keymap.set("n", "<leader>cdb", dapui.close)
 
