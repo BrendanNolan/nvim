94edabde5ea9ec0727546cc2fd68534ec6607315
Brendan Nolan <bnolan23@gmail.com>
2023-01-16 19:17:38 +0000

    Add Window Resizing Bindings

--- lua/brendan/remap.lua
+++ lua/brendan/remap.lua
@@ -41,3 +41,7 @@ vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
+-- Make split resizing easier
+vim.keymap.set("n", "<leader>.", "10<C-w>>")
+vim.keymap.set("n", "<leader>,", "10<C-w><")
+
