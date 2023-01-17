44d7b36931ea81e0687b2e5e147358fc2abfef30
Brendan Nolan <bnolan23@gmail.com>
2023-01-17 19:10:41 +0000

    Customize Telescope

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,13 +1,14 @@
 local builtin = require('telescope.builtin')
 
-vim.keymap.set('n', '<leader>pf', function()
-    builtin.find_files({shorten_path = true})
-end)
-vim.keymap.set('n', '<leader>gf', function()
-    builtin.git_files({shorten_path = true})
-end)
-vim.keymap.set('n', '<leader>ps', function()
-	builtin.grep_string({ search = vim.fn.input("Grep > "), shorten_path = true })
+vim.keymap.set('n', '<leader>pf', builtin.find_files)
+vim.keymap.set('n', '<leader>gf', builtin.git_files)
+vim.keymap.set('n', '<leader>gr', function()
+    builtin.grep_string({ search = vim.fn.input("Grep > ") })
 end)
 vim.keymap.set('n', '<leader>tr', builtin.resume)
 
+require('telescope').setup{
+    defaults = require("telescope.themes").get_dropdown(
+        {layout_config = { width = 120 }, shorten_path = true}
+    )
+}
