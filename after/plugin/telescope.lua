93c5a5b734489c73042546bc87073957b8e3a12c
Brendan Nolan <bnolan23@gmail.com>
2023-01-28 13:00:04 +0000

    Make Telescope Default To Normal Mode

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -9,8 +9,12 @@ vim.keymap.set('n', '<leader>lf', builtin.current_buffer_fuzzy_find)
 vim.keymap.set('n', '<leader>lg', builtin.live_grep)
 vim.keymap.set('n', '<leader>tr', builtin.resume)
 
+
+local my_defaults = require("telescope.themes").get_dropdown(
+    {layout_config = { width = 120 }, shorten_path = true}
+)
+my_defaults.initial_mode = 'normal'
+
 require('telescope').setup{
-    defaults = require("telescope.themes").get_dropdown(
-        {layout_config = { width = 120 }, shorten_path = true}
-    )
+    defaults = my_defaults,
 }
