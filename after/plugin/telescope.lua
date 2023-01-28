fdd875a5ab9570629c279264d0bc559ff2b382b2
Brendan Nolan <bnolan23@gmail.com>
2023-01-28 13:00:37 +0000

    Revert "Make Telescope Default To Normal Mode"
    This reverts commit 79dde756bb63e9538518ca625e0f02a6038ddaeb.

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -9,12 +9,8 @@ vim.keymap.set('n', '<leader>lf', builtin.current_buffer_fuzzy_find)
 vim.keymap.set('n', '<leader>lg', builtin.live_grep)
 vim.keymap.set('n', '<leader>tr', builtin.resume)
 
-
-local my_defaults = require("telescope.themes").get_dropdown(
-    {layout_config = { width = 120 }, shorten_path = true}
-)
-my_defaults.initial_mode = 'normal'
-
 require('telescope').setup{
-    defaults = my_defaults,
+    defaults = require("telescope.themes").get_dropdown(
+        {layout_config = { width = 120 }, shorten_path = true}
+    )
 }
