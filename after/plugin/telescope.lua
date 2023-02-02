23467911ab67bc2111bb558557c794512c431b77
Brendan Nolan <bnolan23@gmail.com>
2023-02-02 20:20:09 +0000

    Revert "Remove Global And buffer Exact Find"
    This reverts commit 54626f54b5274af1473521baf0f0d699d4801b9b.
    The `'` syntax behaviour wasn't quite what I expected.

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,30 +1,17 @@
 local builtin = require('telescope.builtin')
 
-vim.keymap.set('n', '<leader>tr', builtin.resume)
-
 -- File Finding
 -- vim.keymap.set('n', '<leader>pf', builtin.find_files)
 vim.keymap.set('n', '<leader>ff', builtin.git_files)  -- File Find
 
 -- Text Finding
+vim.keymap.set('n', '<leader>bf', function() builtin.current_buffer_fuzzy_find({fuzzy=false}) end)  -- Buffer Find. Note that we need nvim-telescope/telescope-fzf-native.nvim in order to use {fuzzy=false}
+vim.keymap.set('n', '<leader>fbf', builtin.current_buffer_fuzzy_find)  -- Fuzzy Buffer Find
+vim.keymap.set('n', '<leader>gf', builtin.live_grep)  -- Global Find
+vim.keymap.set('n', '<leader>fgf', function() builtin.grep_string{ shorten_path = true, word_match = "-w", only_sort_text = true, search = '' } end)  -- Fuzzy Global Find
+vim.keymap.set('n', '<leader>wf', builtin.grep_string)  -- Word Find: find word under cursor (non fuzzy)
 
--- Buffer Find. Will be fuzzy by default - prefix search string with ' to make it nonfuzzy
--- This non-fuzzy syntax relies on the nvim-telescope/telescope-fzf-native.nvim plugin.
-vim.keymap.set('n', '<leader>bf', builtin.current_buffer_fuzzy_find)
-
-
--- Global Find. Will be fuzzy by default - prefix search string with ' to make it nonfuzzy
--- This non-fuzzy syntax relies on the nvim-telescope/telescope-fzf-native.nvim plugin.
-vim.keymap.set('n', '<leader>gf', function()
-    builtin.grep_string{
-        shorten_path = true,
-        word_match = "-w",
-        only_sort_text = true,
-        search = '' }
-end)
-
--- Word Find: find word under cursor (non fuzzy)
-vim.keymap.set('n', '<leader>wf', builtin.grep_string)
+vim.keymap.set('n', '<leader>tr', builtin.resume)
 
 require('telescope').setup{
     defaults = require("telescope.themes").get_dropdown(
