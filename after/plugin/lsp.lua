16c38d022ad5e49332744380581bd22906b258c6
Brendan Nolan <bnolan23@gmail.com>
2023-02-03 19:44:35 +0000

    Use <C-a> To Abort Cmp Suggestions

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -49,7 +49,7 @@ cmp.setup({
         ['<C-b>'] = cmp.mapping.scroll_docs(-4),
         ['<C-f>'] = cmp.mapping.scroll_docs(4),
         ['<C-Space>'] = cmp.mapping.complete(),
-        ['<C-e>'] = cmp.mapping.abort(),
+        ['<C-a>'] = cmp.mapping.abort(),
         ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
     }),
     sources = cmp.config.sources({
