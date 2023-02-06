70e9c5fc386a8cdca851c3cebee587bda48fdde6
Brendan Nolan <bnolan23@gmail.com>
2023-02-06 14:52:17 +0000

    Improve Floaterm Abilities
    Added keymaps to open/close/toggle, making sure they work both in normal
    and in terminal mode.

---
+++ after/plugin/floaterm.lua
@@ -0,0 +1,15 @@
+vim.keymap.set("t", "<F7>", "<C-\\><C-n><cmd>FloatermNew<CR>")
+vim.keymap.set("n", "<F7>", "<cmd>FloatermNew<CR>")
+
+vim.keymap.set("t", "<F8>", "<C-\\><C-n><cmd>FloatermKill<CR>")
+vim.keymap.set("n", "<F8>", "<cmd>FloatermKill<CR>")
+
+vim.keymap.set("t", "<F9>", "<C-\\><C-n><cmd>FloatermPrev<CR>")
+vim.keymap.set("n", "<F9>", "<cmd>FloatermPrev<CR>")
+
+vim.keymap.set("t", "<F10>", "<C-\\><C-n><cmd>FloatermNext<CR>")
+vim.keymap.set("n", "<F10>", "<cmd>FloatermNext<CR>")
+
+vim.keymap.set("t", "<F12>", "<C-\\><C-n><cmd>FloatermToggle<CR>")
+vim.keymap.set("n", "<F12>", "<cmd>FloatermToggle<CR>")
+
--- after/plugin/git.lua
+++ after/plugin/git.lua
@@ -6,10 +6,10 @@ vim.g.floaterm_width = 0.99
 vim.g.floaterm_height = 0.99
 
 -- Lazygit --
-vim.keymap.set('n', '<leader>lg', ':FloatermNew lazygit<CR>')
+vim.keymap.set('n', '<leader>lg', '<cmd>FloatermNew lazygit<CR>')
 -------------
 
 -- GitUi --
-vim.keymap.set('n', '<leader>gu', ':FloatermNew gitui<CR>')
+vim.keymap.set('n', '<leader>gu', '<cmd>FloatermNew gitui<CR>')
 -------------
 
