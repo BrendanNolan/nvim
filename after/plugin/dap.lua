628b3338b69fce78ac1eb19d5eca9a04187c2328
Brendan Nolan <bnolan23@gmail.com>
2023-03-13 17:04:17 +0000

    Bind <leader>vv To Show Value While Debugging

--- after/plugin/dap.lua
+++ after/plugin/dap.lua
@@ -49,6 +49,9 @@ vim.keymap.set("n", "<leader>cb", function()
     dap.set_breakpoint(vim.fn.input("Breakpoint Condition: "))
 end)
 
+local widgets = require("dap.ui.widgets")
+vim.keymap.set("n", "<leader>vv", widgets.hover)
+
 
 require("nvim-dap-virtual-text").setup()
 
