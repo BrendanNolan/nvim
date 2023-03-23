146d29b752b2777eef87227bc73330a2d9cc0a0f
Brendan Nolan <bnolan23@gmail.com>
2023-03-23 11:02:15 +0000

    Improve Keymappings

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -2,13 +2,15 @@ local builtin = require('telescope.builtin')
 
 -- Lsp
 vim.keymap.set('n', '<leader>rf', builtin.lsp_references, {desc = "Telescope show lsp refs"})
+vim.keymap.set('n', '<leader>tds', builtin.lsp_document_symbols, {desc = "Telescope show document symbols"})
+vim.keymap.set('n', '<leader>tws', builtin.lsp_workspace_symbols, {desc = "Telescope show workspace symbols"})
 
 -- File Finding
 -- vim.keymap.set('n', '<leader>pf', builtin.find_files)
 vim.keymap.set('n', '<leader>ff', builtin.git_files)  -- File Find
 
 -- Text Finding
-vim.keymap.set('n', '<leader>bf', function() builtin.current_buffer_fuzzy_find({fuzzy=false}) end)  -- Buffer Find. Note that we need nvim-telescope/telescope-fzf-native.nvim in order to use {fuzzy=false}
+vim.keymap.set('n', '<leader>fs', function() builtin.current_buffer_fuzzy_find({fuzzy=false}) end)  -- Buffer Find. Note that we need nvim-telescope/telescope-fzf-native.nvim in order to use {fuzzy=false}
 vim.keymap.set('n', '<leader>fbf', builtin.current_buffer_fuzzy_find)  -- Fuzzy Buffer Find
 vim.keymap.set('n', '<leader>gf', builtin.live_grep)  -- Global Find
 vim.keymap.set('n', '<leader>fgf', function() builtin.grep_string{ shorten_path = true, word_match = "-w", only_sort_text = true, search = '' } end)  -- Fuzzy Global Find
