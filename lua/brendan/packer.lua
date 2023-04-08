f10070fe1ad2fcce325f01fa566912cd0a6045df
Brendan Nolan <bnolan23@gmail.com>
2023-04-08 19:23:11 +0100

    Add Treesitter Context
    Shows which function/block you are in at the top of the buffer.

--- lua/brendan/packer.lua
+++ lua/brendan/packer.lua
@@ -46,9 +46,11 @@ return require('packer').startup(function(use)
     use 'Mofiqul/dracula.nvim'
 
     use('nvim-treesitter/nvim-treesitter', { run = 'TSUpdate' })
+    use('nvim-treesitter/nvim-treesitter-context')
     use('nvim-treesitter/nvim-treesitter-refactor')
     use('nvim-treesitter/nvim-treesitter-textobjects')
     use('nvim-treesitter/playground')
+
     use('theprimeagen/harpoon')
     use('mbbill/undotree')
     use('tpope/vim-fugitive')
