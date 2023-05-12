9fffe897a8633fefb6295353dc038d452a5c0f5f
Brendan Nolan <bnolan23@gmail.com>
2023-05-12 07:40:10 +0100

    Make Lualine Show Filepath Relative To Project

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -25,7 +25,7 @@ require('lualine').setup {
   sections = {
     lualine_a = {'mode'},
     lualine_b = {'branch', 'diff', 'diagnostics'},
-    lualine_c = {'filename', {navic.get_location, cond = navic.is_available}},
+    lualine_c = {{'filename', path = 1}, {navic.get_location, cond = navic.is_available}},
     lualine_x = {'encoding', 'fileformat', 'filetype'},
     lualine_y = {'progress'},
     lualine_z = {'location'}
