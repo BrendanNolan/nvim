3c30569a56bd0e436e429435b6f33a93ebe58b97
Brendan Nolan <bnolan23@gmail.com>
2023-05-17 16:11:34 +0100

    Change LSP Format Keybinding To <leader>flsp
    Previous binding of <leader>f was interfering with other more used
    bindings.

--- lua/brendan/remap.lua
+++ lua/brendan/remap.lua
@@ -30,7 +30,7 @@ vim.keymap.set("i", "<C-c>", "<Esc>")
 
 vim.keymap.set("n", "Q", "<nop>")
 vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux new tmux-sessionizer<CR>")
-vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
+vim.keymap.set("n", "<leader>flsp", vim.lsp.buf.format)
 
 -- Quickfix --
 vim.keymap.set("n", "<leader>co", "<cmd>copen<CR>zz")
