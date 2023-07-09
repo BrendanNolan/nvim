f639d7d5404bfa39f89e16e7db3e1c0046310e3f
Brendan Nolan <bnolan23@gmail.com>
2023-07-09 17:13:19 +0100

    Oil.nvim: Skip Confirm For Simple Edits

--- after/plugin/oil.lua
+++ after/plugin/oil.lua
@@ -1,3 +1,6 @@
-require('oil').setup{ keymaps = { ['gh'] = 'actions.toggle_hidden' } }
+require('oil').setup{
+  keymaps = { ['gh'] = 'actions.toggle_hidden' },
+  skip_confirm_for_simple_edits = true,
+}
 
 vim.keymap.set('n', '-', require('oil').open, { desc = 'Open parent directory' })
