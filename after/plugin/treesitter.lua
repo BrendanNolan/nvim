e8a592a4962bf4b649ef7f48c24237c5442c38d0
Brendan Nolan <bnolan23@gmail.com>
2024-05-31 04:51:02 +0100

    Add markdown plugin and required treesitter parsers

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,6 +1,7 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all"
-  ensure_installed = { "javascript", "typescript", "c", "cpp", "lua", "rust" },
+  ensure_installed = {
+    "javascript", "typescript", "c", "cpp", "lua", "rust", "markdown", "markdown_inline" , "latex"},
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
--- lua/brendan/packer.lua
+++ lua/brendan/packer.lua
@@ -120,4 +120,13 @@ return require('packer').startup(function(use)
 
     use { "github/copilot.vim" }
 
+    use {
+        'MeanderingProgrammer/markdown.nvim',
+        as = 'render-markdown', -- Only needed if you have another plugin named markdown.nvim
+        after = { 'nvim-treesitter' },
+        config = function()
+            require('render-markdown').setup({})
+        end,
+    }
+
 end)
