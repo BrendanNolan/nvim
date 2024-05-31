062374b196f9500c8a3ddd2ce25570d4b4877823
Brendan Nolan <bnolan23@gmail.com>
2024-05-31 07:06:33 +0100

    Stop oil.nvim from taking over my pane-switch keymaps

--- after/plugin/oil.lua
+++ after/plugin/oil.lua
@@ -1,6 +1,25 @@
 require('oil').setup{
-  keymaps = { ['gh'] = 'actions.toggle_hidden' },
+  keymaps = {
+    ['gh'] = 'actions.toggle_hidden',
+    ["g?"] = "actions.show_help",
+    -- Only specifying these because the defaults interfere with my own keymaps for pane switching
+    ["<CR>"] = "actions.select",
+    ["loils"] = "actions.select_vsplit",
+    ["loilh"] = "actions.select_split",
+    ["loilt"] = "actions.select_tab",
+    ["loilp"] = "actions.preview",
+    ["loilc"] = "actions.close",
+    ["loill"] = "actions.refresh",
+    ["-"] = "actions.parent",
+    ["_"] = "actions.open_cwd",
+    ["`"] = "actions.cd",
+    ["~"] = "actions.tcd",
+    ["gcs"] = "actions.change_sort",
+    ["gx"] = "actions.open_external",
+    ["g\\"] = "actions.toggle_trash",
+  },
   skip_confirm_for_simple_edits = true,
+  use_default_keymaps = false,
   view_options = { show_hidden = true },
 }
 
