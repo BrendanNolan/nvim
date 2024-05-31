abe6788b2347956625da0d2d7d2cb573f67a5526
Brendan Nolan <bnolan23@gmail.com>
2024-05-31 06:49:47 +0100

    Add good session save/restore keymaps

--- after/plugin/obsess.lua
+++ after/plugin/obsess.lua
@@ -1,2 +1,3 @@
-vim.keymap.set('n', '<leader>vo', '<cmd>Obsess<CR>', {desc="Run :Obsess"})
+vim.keymap.set('n', '<leader>ss', '<cmd>Obsess<CR>', {desc="Obsess Save Session"})
+vim.keymap.set('n', '<leader>sr', '<cmd>source Session.vim<CR>', {desc="Obsess Restore Session"})
 
