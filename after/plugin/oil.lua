7239d83be580ca3b1abdd3dd63c86c4b3e55ee17
Brendan Nolan <bnolan23@gmail.com>
2024-05-31 06:15:44 +0100

    Make oil.nvim show hidden files by default

--- after/plugin/oil.lua
+++ after/plugin/oil.lua
@@ -1,6 +1,7 @@
 require('oil').setup{
   keymaps = { ['gh'] = 'actions.toggle_hidden' },
   skip_confirm_for_simple_edits = true,
+  view_options = { show_hidden = true },
 }
 
 vim.keymap.set('n', '-', require('oil').open, { desc = 'Open parent directory' })
