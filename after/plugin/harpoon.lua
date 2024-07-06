d546ced295e5384a5cc89f6a9a61211a4cce3b4a
Brendan Nolan <bnolan23@gmail.com>
2024-07-06 18:50:21 +0100

    Change the Harpoon Alt Keys To The Left Hand

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -4,8 +4,8 @@ local ui = require("harpoon.ui")
 vim.keymap.set("n", "<leader>a", mark.add_file)
 vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
 
-vim.keymap.set("n", "<A-h>", function() ui.nav_file(1) end)
-vim.keymap.set("n", "<A-j>", function() ui.nav_file(2) end)
-vim.keymap.set("n", "<A-k>", function() ui.nav_file(3) end)
-vim.keymap.set("n", "<A-l>", function() ui.nav_file(4) end)
+vim.keymap.set("n", "<A-a>", function() ui.nav_file(1) end)
+vim.keymap.set("n", "<A-s>", function() ui.nav_file(2) end)
+vim.keymap.set("n", "<A-d>", function() ui.nav_file(3) end)
+vim.keymap.set("n", "<A-f>", function() ui.nav_file(4) end)
 
