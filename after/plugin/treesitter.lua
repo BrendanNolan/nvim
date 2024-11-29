a000e49da7d1ed1c2da27ddbfc7562e33a8eb4b2
Brendan Nolan <bnolan23@gmail.com>
2024-11-29 10:09:53 +0000

    Add more treesitter-text-objects keymaps

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -35,12 +35,42 @@ require'nvim-treesitter.configs'.setup {
       set_jumps = true, -- whether to set jumps in the jumplist
       goto_next_start = {
         ["]f"] = "@function.outer",
-        ["]s"] = "@class.outer",
+        ["]p"] = "@parameter.outer",
+        ["]c"] = { query = "@class.outer", desc = "Next class start" },
+        --
+        -- You can use regex matching (i.e. lua pattern) and/or pass a list in a "query" key to group multiple queries.
+        -- ["]o"] = "@loop.*",
+        ["]o"] = { query = { "@loop.inner", "@loop.outer" } },
+        --
+        -- You can pass a query group to use query from `queries/<lang>/<query_group>.scm file in your runtime path.
+        -- Below example nvim-treesitter's `locals.scm` and `folds.scm`. They also provide highlights.scm and indent.scm.
+        ["]s"] = { query = "@local.scope", query_group = "locals", desc = "Next scope" },
+        ["]z"] = { query = "@fold", query_group = "folds", desc = "Next fold" },
+      },
+      goto_next_end = {
+        ["]]f"] = "@function.outer",
+        ["]]p"] = "@parameter.outer",
+        ["]]c"] = "@class.outer",
       },
       goto_previous_start = {
         ["[f"] = "@function.outer",
-        ["[s"] = "@class.outer",
+        ["[p"] = "@parameter.outer",
+        ["[c"] = "@class.outer",
       },
+      goto_previous_end = {
+        ["[[f"] = "@function.outer",
+        ["[[p"] = "@parameter.outer",
+        ["[[c"] = "@class.outer",
+      },
+      -- Below will go to either the start or the end, whichever is closer.
+      -- Use if you want more granular movements
+      -- Make it even more gradual by adding multiple queries and regex.
+      goto_next = {
+        ["]d"] = "@conditional.outer",
+      },
+      goto_previous = {
+        ["[d"] = "@conditional.outer",
+      }
     },
     select = {
       enable = true,
@@ -58,6 +88,8 @@ require'nvim-treesitter.configs'.setup {
         ["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
         -- You can also use captures from other query groups like `locals.scm`
         ["as"] = { query = "@local.scope", query_group = "locals", desc = "Select language scope" },
+        ["aa"] = "@parameter.outer",
+        ["ia"] = "@parameter.inner",
       },
       -- You can choose the select mode (default is charwise 'v')
       --
